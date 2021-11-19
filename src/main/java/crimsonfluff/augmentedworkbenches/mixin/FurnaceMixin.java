c3b9434525c5678d8a3f443504e3f12dfd0869d1
Waide Soper <waide.soper@gmail.com>
2021-11-19 14:31:59 +1100

    Taking sides!

--- src/main/java/crimsonfluff/augmentedworkbenches/mixin/FurnaceMixin.java
+++ src/main/java/crimsonfluff/augmentedworkbenches/mixin/FurnaceMixin.java
@@ -1,10 +1,7 @@
 package crimsonfluff.augmentedworkbenches.mixin;
 
 import crimsonfluff.augmentedworkbenches.AugmentedWorkbenches;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
-import net.minecraft.block.FluidBlock;
-import net.minecraft.block.FurnaceBlock;
+import net.minecraft.block.*;
 import net.minecraft.block.entity.AbstractFurnaceBlockEntity;
 import net.minecraft.block.entity.FurnaceBlockEntity;
 import net.minecraft.fluid.FluidState;
@@ -13,12 +10,15 @@ import net.minecraft.screen.PropertyDelegate;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.*;
 
 @Mixin(AbstractFurnaceBlockEntity.class)
 public class FurnaceMixin {
@@ -28,17 +28,30 @@ public class FurnaceMixin {
     protected void onFurnaceTick(CallbackInfo ci){
         AbstractFurnaceBlockEntity furnaceBlockEntity = (AbstractFurnaceBlockEntity) (Object) this;
         World world = furnaceBlockEntity.getWorld();
-        BlockPos pos = furnaceBlockEntity.getPos().down();
+        List<BlockPos> posList = Collections.emptyList();
+        posList.add(furnaceBlockEntity.getPos().down());
+        posList.add(furnaceBlockEntity.getPos().up());
+        posList.add(furnaceBlockEntity.getPos().north());
+        posList.add(furnaceBlockEntity.getPos().south());
+        posList.add(furnaceBlockEntity.getPos().east());
+        posList.add(furnaceBlockEntity.getPos().west());
 
-            if(!world.isClient) {
-                if (world.getBlockState(pos).getBlock() instanceof FluidBlock && world.getBlockState(pos).getBlock() == Blocks.LAVA) {
+        if(!world.isClient) {
+            for(BlockPos pos : posList) {
+                BlockState state = world.getBlockState(pos);
+                Block block = state.getBlock();
+                if (block instanceof FluidBlock) {
                     FluidState fluidState = world.getFluidState(pos);
                     int modifier = fluidState.getFluid().getLevel(fluidState);
-                    furnaceBlockEntity.cookTime = MathHelper.clamp(furnaceBlockEntity.cookTime + modifier, 0, furnaceBlockEntity.cookTimeTotal -1);
-                    AugmentedWorkbenches.LOGGER.info(fluidState.getFluid().getLevel(fluidState));
-                    world.markDirty(pos, furnaceBlockEntity);
+                    if (block == Blocks.LAVA) { //a full block smelts at 8 times speed, but consumes fuel at the same rate (8 items, 1 coal, just over a second)
+                        furnaceBlockEntity.cookTime = MathHelper.clamp(furnaceBlockEntity.cookTime + modifier - 1, 0, furnaceBlockEntity.cookTimeTotal - 1);
+                        furnaceBlockEntity.burnTime = MathHelper.clamp(furnaceBlockEntity.burnTime + 1 - modifier, 0, furnaceBlockEntity.fuelTime);
+                    }
+                } else if (block == Blocks.MAGMA_BLOCK) {//smelts at double speed fuel is consumed at half rate (smelt 16 items with 1 coal in 10 seconds in a regular furnace)
+                    furnaceBlockEntity.cookTime = MathHelper.clamp(furnaceBlockEntity.cookTime + 1, 0, furnaceBlockEntity.cookTimeTotal);
                 }
+                world.markDirty(pos, furnaceBlockEntity);
             }
-
+        }
     }
 }
